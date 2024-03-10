6710d0d5c4554823764160a43acdb1c509595f6c
ITACHI <ajogujoseph0317@gmail.com>
2024-03-10 21:18:14 +0100

    made some changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function createHeart() {
   heart.style.left = Math.random() * 100 + "vw";
   heart.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-  heart.innerText = "💗";
+  heart.innerText = "🎈💕";
 
   document.body.appendChild(heart);
 
